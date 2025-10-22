ef061af1e0b1be0541ce8cdb23e543928dcd16d1
Maciej Sikora <maciej.sikora1508@gmail.com>
2025-10-22 18:07:44 +0200

    task solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,69 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'Name',
+  Surname = 'Surname',
+  Age = 'Age',
+  Married = 'Married',
+  AverageGrade = 'AverageGrade',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const studentsCopy = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  const avg = (grades: number[]):
+  number => grades.reduce((sum, g) => sum + g, 0) / grades.length;
+
+  return studentsCopy.sort((a, b) => {
+    let valueA: string | number | boolean;
+    let valueB: string | number | boolean;
+
+    switch (sortBy) {
+      case SortType.Name:
+        valueA = a.name.toLowerCase();
+        valueB = b.name.toLowerCase();
+        break;
+      case SortType.Surname:
+        valueA = a.surname.toLowerCase();
+        valueB = b.surname.toLowerCase();
+        break;
+      case SortType.Age:
+        valueA = a.age;
+        valueB = b.age;
+        break;
+      case SortType.Married:
+        valueA = a.married ? 1 : 0;
+        valueB = b.married ? 1 : 0;
+        break;
+      case SortType.AverageGrade:
+        valueA = avg(a.grades);
+        valueB = avg(b.grades);
+        break;
+      default:
+        return 0;
+    }
+
+    if (valueA < valueB) {
+      return order === 'asc' ? -1 : 1;
+    }
+
+    if (valueA > valueB) {
+      return order === 'asc' ? 1 : -1;
+    }
+
+    return 0;
+  });
 }
